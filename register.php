73a5a241c9e366ea1f090db79dde9528b04955b4
Diell Baholli <diell.w40@gmail.com>
2021-02-12 21:48:36 +0100

    faqja register initial commit

---
+++ register.php
@@ -0,0 +1,106 @@
+<?php
+include_once('function.php');
+
+$userdata=new DB_con();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>DE Digital Marketing | Register</title>
+    <link rel='stylesheet' href='style.css' type='text/css' />
+<header>
+        <div class="container">
+            <div id="branding">
+                <h1><span class="highlight">DE</span>Digital Marketing</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="about.html">ABOUT</a></li>
+                    <li><a href="blog.html">Products</a></li>
+                    <li><a href="sherbimet.html">SHERBIMET</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li class="current"><a href="register.php">Register</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+ <script>
+function checkusername(va) {
+  $.ajax({
+  type: "POST",
+  url: "check_availability.php",
+  data:'username='+va,
+  success: function(data){
+  $("#usernameavailblty").html(data);
+  }
+  });
+
+}
+</script>
+</head>
+
+<div id="formulari">
+        <h1><b>Formulari i Regjistrimit</b></h1>
+    </div>
+<body>
+    <center>
+<form class="form-horizontal" action='' method="POST">
+  <fieldset>
+    
+
+<div class="control-group">
+      <label class="control-label"  for="username">Name</label>
+      <div class="controls">
+        <input type="text" id="username" name="fullname" placeholder="" class="input-xlarge" required="true">
+      </div>
+    </div>
+
+
+    <div class="control-group">
+      <label class="control-label"  for="username">Username</label>
+      <div class="controls">
+<input type="text" id="username" name="username" onblur="checkusername(this.value)" class="input-xlarge" required="true">
+          <span id="usernameavailblty"></span>
+      </div>
+    </div>
+ 
+    <div class="control-group">
+      <label class="control-label" for="email">E-mail</label>
+      <div class="controls">
+        <input type="email" id="email" name="email" placeholder="" class="input-xlarge" required="true">
+      </div>
+    </div>
+ 
+    <div class="control-group">
+      <label class="control-label" for="password">Password</label>
+      <div class="controls">
+        <input type="password" id="password" name="password" placeholder="" class="input-xlarge" required="true">
+      </div>
+    </div>
+ 
+
+    <br>
+    <div class="control-group">
+      <div class="controls">
+        <button class="btn btn-success" type="submit" id="submit" name="submit">Register</button>
+      </div>
+    </div>
+
+ <div class="control-group">
+      <div class="controls">
+       Nese keni account -> <a href="login.php">Signin</a>
+      </div>
+    </div>
+
+  </fieldset>
+</form>
+
+</center>
+<footer>
+        <p>DE Digital Marketing, Copyright &copy; 2020/2021</p>
+    </footer>
+</body>
+</html>
