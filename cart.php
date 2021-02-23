7dca6fcf122c17ba69d798758a462f38095e0e4f
Edon Ibrahimaj <ei50555@ubt-uni.net>
2021-02-23 19:41:11 +0100

    initial commit

---
+++ cart.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Web Projekti | Shopping Cart Produktet</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="style.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	</head>
+	<body>
+    <header>
+        <div class="container">
+            <div id="branding">
+                <h1><span class="highlight">DE</span>Digital Marketing</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.html">HOME</a></li>
+                    <li><a href="about.html">ABOUT</a></li>
+                    <li><a href="blog.html">BLOG</a></li>
+                    <li><a href="sherbimet.html">SHERBIMET</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="Register.php">Register</a></li>
+                    <li class="current"><a href="cart.php">Cart</a></li>
+                    <li><a href="contactus.php">Contact us</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+		<br />
