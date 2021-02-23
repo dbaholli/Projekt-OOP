72a3fb18fb22178c5f0c23ae76e36d7aefb2d4c0
Edon Ibrahimaj <ei50555@ubt-uni.net>
2021-02-23 19:45:24 +0100

    remove produktin

--- cart.php
+++ cart.php
@@ -37,7 +37,18 @@ if(isset($_POST["add_to_cart"]))
 
 if(isset($_GET["action"]))
 {
-	
+	if($_GET["action"] == "delete")
+	{
+		foreach($_SESSION["shopping_cart"] as $keys => $values)
+		{
+			if($values["item_id"] == $_GET["id"])
+			{
+				unset($_SESSION["shopping_cart"][$keys]);
+				echo '<script>alert("Item Removed")</script>';
+				echo '<script>window.location="cart.php"</script>';
+			}
+		}
+	}
 }
 
 ?>
