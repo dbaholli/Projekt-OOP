36a35835e5288f25e3859a08e66d2a09497bf5f8
Diell Baholli <diell.w40@gmail.com>
2021-03-01 15:22:53 +0100

    components code reusability

---
+++ components/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="../style.css" />
+
+</head>
+<body>
+<header>
+        <div class="container">
+            <div id="branding">
+                <h1><span class="highlight">DE</span>Digital Marketing</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php">HOME</a></li>
+                    <li><a href="about.php">ABOUT</a></li>
+                    <li><a href="blog.php">BLOG</a></li>
+                    <li><a href="sherbimet.html">SHERBIMET</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="register.php">Register</a></li>
+                    <!-- <li><a href="cart.php">Cart</a></li> -->
+                    <li><a href="contactus.php">Contact us</a></li>
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
